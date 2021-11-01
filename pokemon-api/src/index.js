b8706d0e97a63243e5dcec9fd2df8a3fae50215c
shaharKamay <shahar.kamay@gmail.com>
2021-11-01 23:51:21 +0200

    testt

--- pokemon-api/src/index.js
+++ pokemon-api/src/index.js
@@ -6,7 +6,7 @@ const pokemonRouter = require('./routers/pokemonRouter');
 const cors = require('cors');
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 
 app.use(cors());
